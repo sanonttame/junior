386300ab5ceb0f34f0ff27c5f356f6dbc0b0c346
sanonttame <amoyst.notna@gmail.com>
2019-03-25 14:53:28 +0300

    3.4. Расстояние между точками в системе координат[#124085]

--- chapter_001/src/main/java/ru/job4j/calculator/package-info.java
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -2,7 +2,7 @@
 * Package for calculator task.
 *
 * @author Anton Narayan 
-* @version 0.1
+* @version 0.3
 * @since 0.1
 */
 package ru.job4j.calculator;
---
+++ chapter_001/src/test/java/condition/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for Condition task.
+*
+* @author Anton Narayan 
+* @version 0.1
+* @since 0.1
+*/
+package condition;
