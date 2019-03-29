32011ef13c3898f586eaa68ce80f1e7c7e057aaa
sanonttame <amoyst.notna@gmail.com>
2019-03-29 22:41:59 +0300

    5.5. Рефакторинг кода.[#124059]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,4 +1,7 @@
 package ru.job4j.loop;
+
+import java.util.function.BiPredicate;
+
 /**
  * Класс строящий пирамиду.
  * @author Anton Narayan
@@ -12,46 +15,41 @@ public class Paint {
      * @param height высота пирамиды.
      *@return объект Доска
      */
-     public String rightTrl(int height) {
+    private String loopBy(int height, int widht, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int width = height;
         for (int x = 0; x != height; x++) {
-            for (int y = 0; y != width; y++) {
-                if (x >= y) {
+            for (int y = 0; y != widht; y++) {
+                if (predict.test(x, y)) {
                     screen.append("^");
-                } else
+                } else {
                     screen.append(" ");
+                }
             }
             screen.append(System.lineSeparator());
         }
         return screen.toString();
     }
-   public String leftTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        int width = height;
-        for (int x = 0; x != height; x++) {
-            for (int y = 0; y != width; y++) {
-                if (x >= width - y- 1) {
-                    screen.append("^");
-                } else
-                    screen.append(" ");
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (x, column) -> x >= column
+        );
     }
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (y, column) -> y >= height - column - 1
+        );
+    }
+
     public String Trl(int height) {
-        StringBuilder screen = new StringBuilder();
-        int width = 2* height - 1;
-        for (int x = 0; x != height; x++) {
-            for (int y = 0; y != width; y++) {
-                if (x >= width - y - 1 && y <= height + x - 1) {
-                    screen.append("^");
-                } else
-                    screen.append(" ");
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (x, y) -> x >= height - y - 1 && x + height - 1 >= y
+        );
     }
+
 }
