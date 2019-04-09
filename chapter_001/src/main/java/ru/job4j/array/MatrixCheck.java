91498c252109a2c042510c178b8bc761fe83a402
sanonttame <amoyst.notna@gmail.com>
2019-04-09 17:45:51 +0300

    Пазлы. Тестовое задание.[#124100]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -17,6 +17,7 @@ public class MatrixCheck {
         for (int i = 0; i < data.length - 1; i++){
             if (data[i][i] != data[i + 1][i + 1] || data[i][data.length - 1 - i] != data[i + 1][data.length - 2 - i]) {
                 result = false;
+                break;
                 }
         }
         return result;
