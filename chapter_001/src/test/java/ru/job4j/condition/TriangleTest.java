4b882a237307ae9102f67022014685f64903835d
sanonttame <amoyst.notna@gmail.com>
2019-04-13 11:37:30 +0300

    Рефакторинг - Площадь треугольника.[#124170]

--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -16,8 +16,11 @@ import static org.junit.Assert.assertThat;
 public class TriangleTest {
     @Test
     public void whenAreaSetThreePointsThenTriangleArea() {
-        Triangle triangle = new Triangle();
-        double result = triangle.area(0, 0, 0, 2, 2, 0);
+        Point first = new Point(0, 0);
+        Point second = new Point(0, 2);
+        Point third = new Point(2, 0);
+        Triangle triangle = new Triangle(first, second, third);
+        double result = triangle.area();
         double expected = 2D;
         assertThat(result, closeTo(expected, 0.1));
     }
