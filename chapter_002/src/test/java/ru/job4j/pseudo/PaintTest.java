674b441a34e188440b4c1eba49c83a22bd5d50c7
sanonttame <amoyst.notna@gmail.com>
2019-05-06 14:46:38 +0300

    4.5. Рефакторинг теста @Before @After[#124130]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -12,30 +14,42 @@ import static org.junit.Assert.assertThat;
  * @since 0.1
  */
 public class PaintTest {
-    @Test
+    private final PrintStream stdout = System.out; // поле содержит дефолтный вывод в консоль.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream(); // буфер для результата.
+    @Before
+    public void loadOutput() {
+         System.setOut(new PrintStream(this.out));
+        }
+        @After
+        public void backOutput() {
+            System.setOut(this.stdout);
+        }
+
+        @Test
     public void whenDrawSquare() {
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфур для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
-        new Paint().draw(new Square());
-        // проверяем результат вычисления
-        assertThat(
-                new String(out.toByteArray()),
-                is(
-                        new StringBuilder()
-                                .append("++++\n")
-                                .append("++++\n")
-                                .append("++++\n")
-                                .append("++++\n")
-                                .append(System.lineSeparator())
-                                .toString()
+         new Paint().draw(new Square()); // выполняем действия пишушиее в консоль.
+             assertThat(new String(out.toByteArray()), is(new StringBuilder()
+                     .append("++++\n")
+                     .append("++++\n")
+                     .append("++++\n")
+                     .append("++++\n")
+                     .append(System.lineSeparator())
+                     .toString()
+             )
+        );
+     }
+    @Test
+    public void whenDrawTriangle() {
+         new Paint().draw(new Triangle());
+        assertThat(out.toString(),
+                is(new StringBuilder()
+                        .append("   *   \n")
+                        .append("  ***  \n")
+                        .append(" ***** \n")
+                        .append("*******\n")
+                        .append(System.lineSeparator())
+                        .toString()
                 )
         );
-        // возвращаем обратно стандартный вывод в консоль.
-        System.setOut(stdout);
     }
 }
