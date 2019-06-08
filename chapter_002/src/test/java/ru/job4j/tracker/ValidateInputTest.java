e9b691b67f4cacad1a5779f193053de44dd699de
sanonttame <amoyst.notna@gmail.com>
2019-06-08 18:00:55 +0300

    6.2.1 Рефакторинг - Шаблон Декоратор для валидатора.[#124138]

--- chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -44,4 +44,17 @@ public class ValidateInputTest {
                 )
         );
     }
+    @Test
+    public void whenInvalidKeyMenu() {
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[] {"7", "1"})
+        );
+        input.ask("Enter", new int[] {1});
+        assertThat(
+                this.mem.toString(),
+                is(
+                        String.format("Please select key from menu.%n")
+                )
+        );
+    }
 }
