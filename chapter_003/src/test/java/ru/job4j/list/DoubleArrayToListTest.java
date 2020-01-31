4bd17f9727a09eee1dd277264c97a41276ed1fa7
sanonttame <amoyst.notna@gmail.com>
2020-01-31 17:43:10 +0300

    Конвертация двухмерный массив в список

---
+++ chapter_003/src/test/java/ru/job4j/list/DoubleArrayToListTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.list;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import ru.job4j.list.DoubleArrayToList;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class DoubleArrayToListTest {
+    @Test
+    public void when2on2ArrayThenList4() {
+        DoubleArrayToList list = new DoubleArrayToList();
+        int[][] input = {
+                {1, 2},
+                {3, 4}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+}
