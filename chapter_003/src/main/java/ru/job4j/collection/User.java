e499898659ed344f3be19e336b36fff5c0463753
sanonttame <amoyst.notna@gmail.com>
2020-02-19 00:57:44 +0300

    Организовать сортировку User

---
+++ chapter_003/src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,33 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    @Override
+    public int compareTo(User user){
+        if (name.compareTo(user.name) == 0){
+            return Integer.compare(age, user.age);
+        } else {
+            return name.compareTo(user.name);
+        }
+
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(name, user.name);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
