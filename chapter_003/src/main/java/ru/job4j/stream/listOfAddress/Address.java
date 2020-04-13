6887943db825f6be2876f4aea5de2a7085a3baac
sanonttame <amoyst.notna@gmail.com>
2020-04-13 07:12:56 +0300

    Список уникальных адресов отсортированных по городу

--- chapter_003/src/main/java/ru/job4j/stream/listOfAddress/Address.java
+++ chapter_003/src/main/java/ru/job4j/stream/listOfAddress/Address.java
@@ -1,11 +1,14 @@
 package ru.job4j.stream.listOfAddress;
+
+import java.util.Objects;
+
 /**
  * class Address
  * @version 0.1
  * @since 0.1
  * @author Anton Tsyoma
  */
-class Address {
+class Address implements Comparable<Address>{
     Address(String city, String street, int home, int apartment) {
         this.city = city;
         this.street = street;
@@ -19,4 +22,41 @@ class Address {
 
     private int apartment;
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, home, apartment);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+       if(this == obj) {
+           return true;
+       }
+       if (obj == null && getClass() != obj.getClass()) {
+           return false;
+       }
+       Address address = (Address) obj;
+       return Objects.equals(city, address.city) &&
+               Objects.equals(street, address.street) &&
+               Objects.equals(home, address.home) &&
+               Objects.equals(apartment, address.apartment);
+    }
+
+    @Override
+    public String toString() {
+        return "\n" + "Address: " + "\n" +
+                "city - " + city + "\n" +
+                "street - " + street + "\n" +
+                "home - " + home + "\n" +
+                "apartment - " + apartment;
+    }
+
+    @Override
+    public int compareTo(Address address) {
+        return this.city.compareTo(address.city);
+    }
+
+    String getCity() {
+        return city;
+    }
 }
--- chapter_003/src/main/java/ru/job4j/stream/listOfAddress/Profiles.java
+++ chapter_003/src/main/java/ru/job4j/stream/listOfAddress/Profiles.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 /**
  * class Profiles
- * @version 0.1
+ * @version 0.2
  * @since 0.1
  * @author Anton Tsyoma
  */
@@ -15,6 +15,20 @@ class Profiles {
      * @return list of address
      */
     List<Address> collect(List<Profile> profiles){
-        return profiles.stream().map((profile)->profile.getAddress()).collect(Collectors.toList());
+        return profiles.stream()
+                .map((profile)->profile.getAddress())
+                .collect(Collectors.toList());
+    }
+    /**
+     * Collect unique addresses sorted by city
+     * @param profiles profiles
+     * @return list of address
+     */
+    List<Address> collectUniqueSortedByCity(List<Profile> profiles) {
+        return profiles.stream()
+                .map((profile)->profile.getAddress())
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
